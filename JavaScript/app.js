e8ae13c549d9fab95560deeb43d95992505a0e60
Mehrabios <mohamadhosin.anbarloo@gmail.com>
2024-01-15 18:04:03 +0330

    Numbers History Added

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,13 @@
 let guessNumberElement = document.querySelector(".guess-number");
-let remainingGuessesElement = document.querySelector(".remaining-guesses")
+let remainingGuessesElement = document.querySelector(".remaining-guesses");
 let formElement = document.querySelector("form");
 let numberInputElement = document.querySelector(".number-input");
 let numberBtnElement = document.querySelector(".number-btn");
-let numberComparisonParagraphElement = document.querySelector(".number-comparison-paragraph")
-let selectInputElement = document.querySelector(".select-input")
+let numberComparisonParagraphElement = document.querySelector(".number-comparison-paragraph");
+let selectInputElement = document.querySelector(".select-input");
+let restartPageElement = document.querySelector(".restart-page");
+let restartBtnElement = document.querySelector(".restart-btn");
+let previousNumbersElement = document.querySelector(".previous-numbers");
 
 function randomNumberSet() {
     return Math.round(Math.random() * 98 + 1);
@@ -30,7 +33,7 @@ function difficultyChecker(){
     }
     remainingGuessesElement.classList.replace("d-none","d-block");
     formElement.classList.replace("d-none","d-block");
-    numberInputElement.value = ""
+    numberInputElement.value = "";
     numberComparisonParagraphElement.textContent = "";
     numberComparisonParagraphElement.classList.remove("color-green");
     numberComparisonParagraphElement.classList.remove("color-red");
@@ -40,28 +43,47 @@ function difficultyChecker(){
 
 function numberChecker(event) {
     event.preventDefault();
-    if (numberInputElement.value < 100 && numberInputElement.value > 1 && guessNumberElement.textContent >= 0){
+    let previousNumbersItemElement = `<div class="previous-numbers-item">${numberInputElement.value}</div>`;
+
+    if (numberInputElement.value < 100 && numberInputElement.value > 1 && guessNumberElement.textContent > 0){
+        
         if (numberInputElement.value > randomNumber){
-            numberComparisonParagraphElement.innerHTML = "Big";
+            numberComparisonParagraphElement.textContent = "Your Number is Big";
             numberComparisonParagraphElement.classList.remove("color-green");
             numberComparisonParagraphElement.classList.remove("color-yellow");
             numberComparisonParagraphElement.classList.add("color-red");
         }
         else if (numberInputElement.value < randomNumber){
-            numberComparisonParagraphElement.innerHTML = "Small";
+            numberComparisonParagraphElement.textContent = "Your Number is Small";
             numberComparisonParagraphElement.classList.remove("color-red");
             numberComparisonParagraphElement.classList.remove("color-green");
             numberComparisonParagraphElement.classList.add("color-yellow");
         }
         else if (numberInputElement.value == randomNumber){
-            numberComparisonParagraphElement.innerHTML = "You Win!";
+            numberComparisonParagraphElement.textContent = "You Win!";
             numberComparisonParagraphElement.classList.remove("color-yellow");
             numberComparisonParagraphElement.classList.remove("color-red");
             numberComparisonParagraphElement.classList.add("color-green");
             numberInputElement.setAttribute("disabled", "");
             numberBtnElement.setAttribute("disabled", "");
+            selectInputElement.setAttribute("disabled", "");
+            restartPageElement.classList.replace("d-none","d-block");
+            restartBtnElement.classList.add("back-green");
         }
-        guessNumberElement.innerHTML--
+        guessNumberElement.innerHTML--;
+
+        if (guessNumberElement.textContent == 0){
+            numberComparisonParagraphElement.textContent = "You Lose!";
+            numberComparisonParagraphElement.classList.remove("color-yellow");
+            numberComparisonParagraphElement.classList.remove("color-green");
+            numberComparisonParagraphElement.classList.add("color-red");
+            numberInputElement.setAttribute("disabled", "");
+            numberBtnElement.setAttribute("disabled", "");
+            restartPageElement.classList.replace("d-none","d-block");
+            restartBtnElement.classList.add("back-red");
+        }
+
+        previousNumbersElement.innerHTML += previousNumbersItemElement;
     }
     else {
         numberComparisonParagraphElement.innerHTML = "Invalid Input!";
